5f9f45f7f1e156c283133cdc4fb48ac29b1300a2
Vinicius Datti <viniciusdatti@gmail.com>
2026-05-30 02:37:35 -0300

    refactor(web): split ProjectShowcase into focused subcomponents
    Move case panel, card, grid, and terminal mock into dedicated folders with colocated tests and types.

--- interfaces/web/src/components/ProjectShowcase/index.ts
+++ interfaces/web/src/components/ProjectShowcase/index.ts
@@ -1,8 +1,6 @@
-// Component
+export * from './ProjectShowcase.types';
+
 export * from './ProjectShowcaseGrid';
 export * from './ProjectShowcaseCard';
 export * from './ProjectTerminalMock';
 export * from './ProjectCasePanel';
-export * from './ProjectCasePanel.types';
-export * from './ProjectCaseStudyContent';
-export * from './ProjectShowcase.types';
